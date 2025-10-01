320e919141747534edd68077212bd514d4041a2c
Pankaj Singla <pankajsingla012@gmail.com>
2025-10-01 23:04:39 +0530

    Commit-001 : Initial Commit Project Created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
